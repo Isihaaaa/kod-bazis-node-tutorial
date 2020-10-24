f078f2a2a2e8a715212228a514b5cc7b9d452e38
Isihaaaa <hajdu.istvan05@gmail.com>
2020-10-24 15:00:25 +0200

    04

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // wrapper function
 // function(__dirname, __filename, module, require, exports){
+const http = require("http");
+const fs = require("fs");
 
 console.log(__dirname);
 console.log(__filename);
 
 const add = require("./calculator");
 console.log(add(2, 3));
-
-const http = require("http");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -16,19 +16,26 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   switch (true) {
     case req.url === "/" && req.method === "GET":
-      res.setHeader("content-type", "text/html; charset=utf-8");
-      res.writeHead(200);
-      res.end('<h1>Login Page</h1> <a href="/login">Cover Page</a>');
+      fs.readFile(__dirname + "/home.html", (err, data) => {
+        res.setHeader("content-type", "text/html; charset=utf-8");
+        res.writeHead(200);
+        res.end(data);
+      });
       break;
     case req.url === "/login" && req.method === "GET":
-      res.setHeader("content-type", "text/html; charset=utf-8");
-      res.writeHead(200);
-      res.end('<h1>Cover Page</h1>  <a href="/">Login</a>');
+      fs.readFile(__dirname + "/login.html", (err, data) => {
+        res.setHeader("content-type", "text/html; charset=utf-8");
+        res.writeHead(200);
+        res.end(data);
+      });
+
       break;
     default:
-      res.setHeader("content-type", "text/html; charset=utf-8");
-      res.writeHead(404);
-      res.end("Page not found!!");
+      fs.readFile(__dirname + "/404.html", (err, data) => {
+        res.setHeader("content-type", "text/html; charset=utf-8");
+        res.writeHead(404);
+        res.end(data);
+      });
   }
 });
 
